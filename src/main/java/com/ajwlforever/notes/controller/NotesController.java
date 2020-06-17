6fc1f73613c1d4e9e36aaca04a27cfe88c231f19
ajwlforever <2353350597@qq.com>
2020-06-17 17:06:32 +0800

    version 1.3 创建笔记本、笔记本，修改笔记完成

--- src/main/java/com/ajwlforever/notes/controller/NotesController.java
+++ src/main/java/com/ajwlforever/notes/controller/NotesController.java
@@ -1,13 +1,16 @@
 package com.ajwlforever.notes.controller;
 
 import com.ajwlforever.notes.annotation.LoginRequired;
+import com.ajwlforever.notes.entity.Note;
 import com.ajwlforever.notes.entity.Notebook;
+import com.ajwlforever.notes.service.NoteService;
 import com.ajwlforever.notes.service.NotebookService;
 import com.ajwlforever.notes.utils.CloudNotesUtil;
 import com.ajwlforever.notes.utils.NoteResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,15 +24,16 @@ public class NotesController {
 
     @Autowired
     private NotebookService notebookService;
-
+    @Autowired
+    private NoteService noteService;
     @LoginRequired
-    @RequestMapping(path = "/edit",method = RequestMethod.GET)
+    @RequestMapping(path = {"/edit","/"},method = RequestMethod.GET)
     public String edit(Model model)
     {
         return "/edit";
     }
 
-
+    @LoginRequired
     @RequestMapping(path ="/book/loadbooks",method = RequestMethod.POST)
     @ResponseBody
     public NoteResult<List<Notebook>> loadBookList(  HttpServletRequest request)
@@ -39,6 +43,7 @@ public class NotesController {
         return  result;
     }
 
+    @LoginRequired
     @RequestMapping(path = "/book/add" ,method = RequestMethod.POST)
     @ResponseBody
     public NoteResult<String> creatABook(String bookName,HttpServletRequest request)
@@ -46,12 +51,64 @@ public class NotesController {
         String token = CloudNotesUtil.getCookies(request,"token");
         NoteResult<String> result = notebookService.createABook(token,bookName);
         return  result;
+    }
+    @LoginRequired
+    @RequestMapping(path = "/loadNotes" , method = RequestMethod.POST)
+    @ResponseBody
+    public NoteResult<List<Note>> loadNotes(String bookId )
+    {
+
+        List<Note> noteList = noteService.selectByNotebookId(bookId);
+        NoteResult<List<Note>> result = new NoteResult<>();
+        result.setStatus(0);
+        result.setMsg("笔记查询成功");
+        result.setData(noteList);
+        return  result;
+    }
+
+    @LoginRequired
+    @RequestMapping(path ="/loadNoteContent",method = RequestMethod.POST)
+    @ResponseBody
+    public NoteResult<Note> loadNoteContent(String noteId)
+    {
+        Note note = noteService.findById(noteId);
+        NoteResult<Note> result = new NoteResult<>();
+
+        result.setStatus(0);
+        result.setMsg("加载笔记内容成功");
+        result.setData(note);
+
+        return  result;
 
 
     }
 
+    @LoginRequired
+    @RequestMapping(path ="/addNote",method = RequestMethod.POST)
+    @ResponseBody
+    public NoteResult<Note> addNote(String noteName, String bookId, @CookieValue("token")String token)
+    {
+        String userId = token.split(":",2)[1];
+        Note note = noteService.createNoteByName(noteName,bookId,userId);
+        NoteResult<Note> result = new NoteResult<>();
+        result.setStatus(0);
+        result.setMsg("创建笔记成功");
+        result.setData(note);
 
-//    @RequestMapping(path ="/notes",method = RequestMethod.POST)
-//    @ResponseBody
-//    public NoteResult
+        return result;
+    }
+
+    @LoginRequired
+    @RequestMapping(path ="/saveNote",method = RequestMethod.POST)
+    @ResponseBody
+    public NoteResult<String> saveNote(String noteId,String noteTitle,String noteBody)
+    {
+        noteService.updateNote(noteId,noteTitle,noteBody,System.currentTimeMillis());
+        NoteResult<String> result = new NoteResult<>();
+        result.setStatus(0);
+        result.setMsg("保存笔记成功!");
+        return  result;
+    }
+
+
 }
